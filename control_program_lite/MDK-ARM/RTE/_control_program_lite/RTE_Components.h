82d59db1d604ad66027984baa492b3672eab1695
BobLouis <bob900316@gmail.com>
2021-11-24 23:48:59 +0800

    first_commit

---
+++ control_program_lite/MDK-ARM/RTE/_control_program_lite/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'control_program_lite' 
+ * Target:  'control_program_lite' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
